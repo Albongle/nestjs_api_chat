87c773dc87e84f5a9d8c27f538e2ed5b1b660e62
Alejandro Bongioanni <ANBongioanni@teco.com.ar>
2023-10-02 22:55:09 -0300

    se agregan filtros

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { Chat } from './chat.model';
 
@@ -8,6 +15,28 @@ export class ChatController {
   @Get()
   public obtenerTodosLosMensajes() {
     return this.chatService.obtenerTodosLosChats();
+  }
+
+  @Get('/buscar')
+  public obtenerMensajesPorEmailEmisor(
+    @Query('emailEmisor') emailEmisor: string,
+    @Query('emailReceptor') emailReceptor: string,
+  ) {
+    if (!emailEmisor && !emailReceptor) {
+      throw new BadRequestException(
+        'Parametros de busqueda requeridos [emailEmisor o emailReceptor]',
+      );
+    }
+
+    if (emailEmisor && emailReceptor) {
+      return this.chatService.obtenerChatsPorEmailEmisorYEmailReceptor(
+        emailEmisor,
+        emailReceptor,
+      );
+    } else if (emailReceptor) {
+      return this.chatService.obtenerChatsPorEmailReceptor(emailReceptor);
+    }
+    return this.chatService.obtenerChatsPorEmailEmisor(emailEmisor);
   }
 
   @Post()
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -13,7 +13,7 @@ export class ChatService {
   ) {}
 
   public async obtenerTodosLosChats(): Promise<ChatResponse> {
-    const chats = await this.chatModel.find();
+    const chats = await this.chatModel.find().exec();
     return plainToInstance(
       ChatResponse,
       { chats: chats },
@@ -25,5 +25,41 @@ export class ChatService {
     const nuevoChat = new this.chatModel(chat);
     const chatCreado = nuevoChat.save();
     return plainToInstance(Chat, chatCreado, { excludeExtraneousValues: true });
+  }
+
+  public obtenerChatsPorEmailEmisor(email: string) {
+    return this.obtenerChatsPorEmailDeUnUsuario(`emisor.email`, email);
+  }
+
+  public obtenerChatsPorEmailReceptor(email: string) {
+    return this.obtenerChatsPorEmailDeUnUsuario(`receptor.email`, email);
+  }
+
+  public async obtenerChatsPorEmailEmisorYEmailReceptor(
+    emailEmisor: string,
+    emailReceptor: string,
+  ) {
+    const chats = await this.chatModel
+      .find({ 'emisor.email': emailEmisor, 'receptor.email': emailReceptor })
+      .exec();
+
+    return plainToInstance(
+      ChatResponse,
+      { chats: chats },
+      { excludeExtraneousValues: true },
+    );
+  }
+
+  private async obtenerChatsPorEmailDeUnUsuario(
+    usuario: string,
+    email: string,
+  ) {
+    const chats = await this.chatModel.find({ [usuario]: email }).exec();
+
+    return plainToInstance(
+      ChatResponse,
+      { chats: chats },
+      { excludeExtraneousValues: true },
+    );
   }
 }
